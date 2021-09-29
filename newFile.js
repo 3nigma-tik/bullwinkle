38db4ad6ed90049eca53ccb961530ba4f1fba6b0
3nigma-tik <cole.blackwood@icloud.com>
2021-09-29 09:16:05 -0700

    Check Check

---
+++ newFile.js
@@ -0,0 +1,5 @@
+//when user criteria is entered, return a list of people that have that single trait
+//user is prompted to enter a trait (provide list of traits?); user enters trait;
+//console logs all people who have that trait; people who have that trait coded in objects;
+//function should cycle through array of objects(people), sort, and display new array of people
+//containing that trait.
