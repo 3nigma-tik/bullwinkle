232159dcfe8c4427a867a1e3602dbc1166e9c5ec
3nigma-tik <cole.blackwood@icloud.com>
2021-11-01 14:20:13 -0700

    EOD

--- PythonProjects/Music/music_library/music/urls.py
+++ PythonProjects/Music/music_library/music/urls.py
@@ -3,6 +3,5 @@ from . import views
 
 urlpatterns = [
     path('music/', views.SongList.as_view()),
-    path('127.0.0.1:8000/music/'),
-    path('127.0.0.1:8000/music/<int:pk>/')
+    path('music/<int:id>/', views.SongList.as_view())
 ]
